14ef3733f8f8f9b0a51e9182a2819b02f59b668e
KRVaghani <kaushik.vaghani111@gmail.com>
2021-01-28 16:41:30 +0530

    css file added and file path define

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.set('views',path.join(__dirname,'views'));//tell the views directory name 
+app.set('view engine','ejs')//this line though we are not every time define .ejs in file
+app.use(express.static('public'));//tell the express we have used this public directory to used css,images,js files
+
 app.get('/user/:id/:username',(req,res)=>{
 
     let userId = req.params.id;
     let user = req.params.username;
-    res.render('index.ejs',{id: userId, username: user});
+    res.render('index',{id: userId, username: user});
 });
 
 app.listen(3000,()=>{
